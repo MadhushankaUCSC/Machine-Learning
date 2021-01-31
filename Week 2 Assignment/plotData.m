77836a9c465169ccc5af06c8aaa5a9e458c1c4b2
MadhushankaUCSC <buthsaramadhushanka@gmail.com>
2021-01-31 21:30:26 +0530

    others

---
+++ Week 2 Assignment/plotData.m
@@ -0,0 +1,26 @@
+function plotData(x, y)
+plot(x, y, 'rx', 'MarkerSize', 10);   %PLOTDATA Plots the data points x and y into a new figure 
+ylabel('Profit in $10,000s');     % PLOTDATA(x,y) plots the data points and gives the figure axes labels of
+xlabel('Population of City in 10,000s');
+%   population and profit.
+
+figure; % open a new figure window
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Plot the training data into a figure using the 
+%               "figure" and "plot" commands. Set the axes labels using
+%               the "xlabel" and "ylabel" commands. Assume the 
+%               population and revenue data have been passed in
+%               as the x and y arguments of this function.
+%
+% Hint: You can use the 'rx' option with plot to have the markers
+%       appear as red crosses. Furthermore, you can make the
+%       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
+
+
+
+
+
+% ============================================================
+
+end
